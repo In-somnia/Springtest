89edbba1ca2b43df4799f8aa3c86c7d3c0473f8a
In-somnia <kuzkina.julia@gmail.com>
2017-05-08 22:39:45 +0300

    Spring ver 4.1.6

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,11 +2,16 @@ public class App {
     private Client client;
     private ConsoleEventLogger eventLogger;
 
-    public static void main(String[] args) {
-        App app = new App();
+    public App(Client client, ConsoleEventLogger eventLogger) {
+        this.client = client;
+        this.eventLogger = eventLogger;
+    }
 
-        app.client = new Client(1, "John Smith");
-        app.eventLogger = new ConsoleEventLogger();
+    public static void main(String[] args) {
+        App app = new App(client, eventLogger);
+
+        /*app.client = new Client(1, "John Smith");
+        app.eventLogger = new ConsoleEventLogger();*/
 
         app.logEvent("Some event for user 1");
     }
